a8c25448b8f7e0375b09a2628d69c92f614836cd
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-02-10 13:56:56 +0000

    working on the userAuthentication

---
+++ api/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const app = require("./app.js");
+const { connectToDatabase } = require("./db/db.js");
+
+const listenForRequests = () => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log("Now listening on port", port);
+  });
+};
+
+connectToDatabase().then(() => {
+  listenForRequests();
+});
