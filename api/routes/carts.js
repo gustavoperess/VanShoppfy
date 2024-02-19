4f3a1af396af55494edf3d4b780e10f89892fba4
Gustavo Moreira <32426662+gustavoperess@users.noreply.github.com>
2024-02-19 10:41:53 +0000

    Update carts.js

--- api/routes/carts.js
+++ api/routes/carts.js
@@ -5,8 +5,8 @@ const { generateUniqueId } = require('./utils');
 
 router.use((req, res, next) => {
     if (!req.cookies.sessionId) {
-      const sessionId = generateUniqueId(); // Implement this function
-      res.cookie('sessionId', sessionId, { httpOnly: true, sameSite: 'Lax', maxAge: 24 * 60 * 60 * 1000 }); // Adjust options as needed
+      const sessionId = generateUniqueId(); 
+      res.cookie('sessionId', sessionId, { httpOnly: true, sameSite: 'Lax', maxAge: 24 * 60 * 60 * 1000 }); // this is how long the cookies will be saved. 1 day currently 
       req.cookies.sessionId = sessionId;
     }
     next();
