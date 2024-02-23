0561bb5bad0f400498ad46bd659ff35f4171d0bd
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-02-23 16:48:05 +0000

    canvas added

--- frontend/src/components/SideBar/sideeBarCard.component.jsx
+++ frontend/src/components/SideBar/sideeBarCard.component.jsx
@@ -1,7 +1,16 @@
 import { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { useCart } from '../../contexts/CartContext';
+import "./sideBarStyle.css"
+
+const options = [
+  {
+    name: 'Enable backdrop (default)',
+    scroll: true,
+    backdrop: true,
+  },
+];
 
 function SidebarComponent() {
   const [show, setShow] = useState(false);
@@ -9,34 +18,46 @@ function SidebarComponent() {
   const [prevCartCount, setPrevCartCount] = useState(cartCount);
   const handleClose = () => setShow(false);
 
-  useEffect(() =>{
+  useEffect(() => {
+    let isMounted = true;
     if (cartCount > prevCartCount) {
-      setShow(true)
+      setShow(true);
     }
-    setPrevCartCount(cartCount);
-  }, [cartItems, prevCartCount])
+    if (isMounted) {
+      setPrevCartCount(cartCount);
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, [cartCount, prevCartCount]);
   
   return (
-       <> 
-      <Offcanvas placement='end' show={show} onHide={handleClose} >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Cart Item(s) {cartCount}</Offcanvas.Title>
-        </Offcanvas.Header>
-        {cartItems.map((item, index) => 
-        <Offcanvas.Body key={index} >
-              {item.productName}
-              {item.productGender}
-        </Offcanvas.Body>
-              )}
-      </Offcanvas>
+    <> 
+      {options.map((props, idx) => (
+        <Offcanvas placement='end' show={show} key={idx} {...props} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Cart Item(s) {cartCount}</Offcanvas.Title>
+          </Offcanvas.Header>
+          {cartItems.map((item, index) => (
+            <div className="offcanvas-body-item" key={item.id || index}>
+              <Offcanvas.Body>
+                <h1>Hello</h1>
+              </Offcanvas.Body>
+            </div>
+          ))}
+        </Offcanvas>
+      ))}
     </>
   );
 }
 
 
-
-
-
-
-
 export default SidebarComponent;
+
+
+{/* <Card className='card-container-canvas'>
+<Card.Img className='canvas-card-img' src={item?.productPicture ? item?.productPicture : 'default-picture-url'} alt={item.productName} />
+
+    <h1>{item.productName}</h1>
+
+</Card> */}
