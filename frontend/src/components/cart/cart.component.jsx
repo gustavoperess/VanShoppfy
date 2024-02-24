8098c5cc865b283870a47c2d7337a22da8ad39e4
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-02-24 20:14:39 +0000

    table in progress

--- frontend/src/components/cart/cart.component.jsx
+++ frontend/src/components/cart/cart.component.jsx
@@ -6,27 +6,8 @@ import Button from 'react-bootstrap/Button';
 import { useCart } from '../../contexts/CartContext';
 
 function CartComponent() {
-    const [products, setProduct] = useState([])
     const { cartItems, cartCount, totalAmount, removeFromCart } = useCart();
 
-useEffect(() =>{
-    const fetchData = async () => {
-        try {
-            let productsData = await getProductBySessionId();
-            productsData = productsData.map((product) => ({
-                ...product,
-                product: {
-                    ...product.product,
-                    productPrice: product.product.productPrice.$numberDecimal ? parseFloat(product.product.productPrice.$numberDecimal) : product.product.productPrice,
-                }
-            }));
-            setProduct(productsData);
-        } catch (err) {
-            console.error('Error fetching products information:', err);
-        }
-    }
-    fetchData()
-}, []);
 
 const formatPrice = (price) => {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'GBP' }).format(price);
@@ -50,18 +31,25 @@ return (
                     <th>Total</th>
                     </tr>
                 </thead>
-                {products.map ((product, index) =>
-                <tbody key={index}>
+                {cartItems.map ((product, index) =>
+                <tbody key={index} className="table-body">
                     <tr>
-                        <td> {product.product.productCategory} </td>
-                        <td></td>
-                        <td>{formatPrice(product.product.productPrice)}</td>
+                        <td className="table-body-product"> 
+                        <img className="myImage" src={product?.productPicture ? product?.productPicture : 'default-picture-url'} alt={product.productPicture} />
+                            <div className="table-body-product-text">
+                                <h1>{product.productName}</h1>
+                                <p>{product.productCategory}</p>
+                                <p>{product.productGender}</p>
+                            </div>     
+                        </td>
+                        <td>{product.quantity}</td>
+                        <td className="table-body-price">{formatPrice(product.productPrice)}</td>
                     </tr>
                 </tbody>
                 )}
                 <tbody>
-                    <tr> 
-                        <td colSpan={3}>{formatPrice(totalAmount)}</td>
+                    <tr className="table-total"> 
+                         <td colSpan={3}><p>Subtotal: {formatPrice(totalAmount)}</p></td>
                     </tr>
                 </tbody>
             </Table>
