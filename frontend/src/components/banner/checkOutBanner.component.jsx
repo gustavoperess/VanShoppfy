6e4251274ef2b7337a0a61790a58d08dd241e21d
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-02-24 17:49:57 +0000

    cart page created

---
+++ frontend/src/components/banner/checkOutBanner.component.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import styled from 'styled-components';
+import "./singleBannerStyle.css";
+
+const myPicture = [
+  {
+    label: "Thanks for shopping with us",
+  },
+];
+
+function CheckOutBannerComponent() {
+  const [pictures, setPictures] = useState([]);
+
+  useEffect(() => {
+    setPictures(myPicture);
+  }, []);
+
+  return (
+    <div className='singleBannerComponent'>
+    <CardLayout>
+      {pictures.map((picture, index) => (
+        <CardThree key={index}>
+           <Card.Title className="custom-card-title">{picture.label}</Card.Title>
+           <Card.Text className="custom-card-text">
+            {picture.description}
+            </Card.Text>
+        </CardThree>
+      ))}
+    </CardLayout>
+    </div>
+  );
+}
+
+const CardLayout = styled.div`
+  margin: 0px 0px 1rem;
+`;
+
+
+const CardThree = styled.div`
+  background: url(../../../src/assets/myBanners/MyCheckoutBanner.jpg) ;
+  height: 300px;
+  display: flex;
+  flex-direction: column; /* This ensures children are stacked vertically */
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+
+`;
+
+export default CheckOutBannerComponent;
--- frontend/src/pages/Cart/cartPage.jsx
+++ frontend/src/pages/Cart/cartPage.jsx
@@ -2,13 +2,14 @@ import CartComponent from "../../components/cart/cart.component"
 import HeaderComponent from '../../components/header/header.component';
 import Container from 'react-bootstrap/Container';
 import FooterComponent from '../../components/footer/footer.component';
-
+import CheckOutBannerComponent from "../../components/banner/checkOutBanner.component";
 
 export const CartPage = () => {
     return (
         <>
-        <Container>
+        <Container fluid>
             <HeaderComponent />
+            <CheckOutBannerComponent />
             <CartComponent />
             <FooterComponent />
         </Container>
