d03acd1dd061af15ed737894ef8ef680a07909e3
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-04-03 13:43:52 +0100

    changes to the style

--- frontend/src/components/profile/profleLatestOrder.component.jsx
+++ frontend/src/components/profile/profleLatestOrder.component.jsx
@@ -44,16 +44,15 @@ function ProfileLatestOrder() {
             <SideListComponent />
           </div>
           {userOrder ? (
-            <div> 
+            <div className='latest-order'> 
                 <h4>Vanshopfy</h4>
                 <h6>YOUR ORDER'S ON ITS WAY</h6>
-                Order number: {userOrder.order?.orderNumber}
+                <p>Order number: {userOrder.order?.orderNumber}</p>
                 <p>Hi {userOrder.order?.name} ,</p> 
                 <p>Your items from your order {userOrder.order?.orderNumber} will be delivered by DHL Express on Monday, Jun 27th, 2022. </p> 
-                Here’s what will arrive:
-
-                Thanks again for shopping with us.
-                Vanshopfy
+                <p>Here’s what will arrive:</p>
+                <p>Thanks again for shopping with us.</p>
+            
                 {userOrder.products?.map((product, index) => (
                         <tbody key={index} className="table-body">
                             <tr>
@@ -74,9 +73,10 @@ function ProfileLatestOrder() {
                             <td colSpan={3}><p>Subtotal: {formatPrice(userOrder.order?.totalAmount)}</p></td>
                         </tr>
                     </tbody>
-                Your delivery information
-                {userOrder.order?.name} 
-                {userOrder.order?.zip} {userOrder.order?.address} {userOrder.order?.city} {userOrder.order?.country}
+                <h6>Your delivery information</h6>
+                <p>{userOrder.order?.name} </p>
+                <p>{userOrder.order?.zip} {userOrder.order?.address} {userOrder.order?.city} </p>
+                <p>{userOrder.order?.country}</p>
    
             </div>
     
