edea3d0463dbdbe5bf0e18c57018f0779687bee8
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-04-03 15:08:15 +0100

    updating the contact page

--- frontend/src/components/contact/contact.component.jsx
+++ frontend/src/components/contact/contact.component.jsx
@@ -11,6 +11,20 @@ function ContactComponent() {
           <Card.Text>
               Have a question or want to work together
           </Card.Text>
+          <div className='about-me'>
+          <p>  Software engineer eager to continue my journey and apply what I’ve learned so far to a team dynamic, 
+          working towards challenging projects. I moved to London, UK from Vancouver, Canada in 2023 and am a recent graduate
+           from Makers Academy. In my 4 years of professional experience in the finance industry, I have proven my ability to 
+           problem solve and communicate effectively with internal team members and external customers. I have always been interested in the 
+           tech industry as it is continuously evolving, which provides me with new challenges and allows me to continuously learn. 
+           I am a quick learner and have a unique blend of technical expertise and interpersonal 
+           skills which will allow me to make a positive impact and succeed in a software development role.</p>
+          </div>
+          <div className='contact-info'>
+            <p>Email: </p>
+            <p>linkedin: </p>
+            <p>github: </p>
+          </div>
           <Link to="#" className="btn btn-dark">Contact Us</Link>      
         </Card.Body>
         <hr></hr>
--- frontend/src/pages/Contact/ContactPage.jsx
+++ frontend/src/pages/Contact/ContactPage.jsx
@@ -2,16 +2,18 @@ import HeaderComponent from '../../components/header/header.component';
 import FooterComponent from '../../components/footer/footer.component';
 import ContactComponent from '../../components/contact/contact.component';
 import Container from 'react-bootstrap/Container';
+import "./ContactPageStyle.css"
 
 
 export const ContactPage = () => {
     return (
-        <Container>
-            <HeaderComponent />
-            <h1>HELLO</h1>
-            <ContactComponent />
-            <FooterComponent />
-        </Container>
-    )
+      
+            <Container >
+                <HeaderComponent />
+                <ContactComponent />
+                <FooterComponent />
+            </Container>
+ 
+    );
 }
 
