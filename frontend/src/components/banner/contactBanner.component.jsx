1fb7cb653c0e2f1724ac2f3361107c32ced7f752
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-04-03 16:06:10 +0100

    contact page completed

---
+++ frontend/src/components/banner/contactBanner.component.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import styled from 'styled-components';
+import "./singleBannerStyle.css";
+
+const myPicture = [
+    {
+      label: "I appreciate your contact",
+    },
+  ];
+
+function ContactBanner() {
+  const [pictures, setPictures] = useState([]);
+
+  useEffect(() => {
+    setPictures(myPicture);
+  }, []);
+
+  return (
+    <div className='singleBannerComponent'>
+    <CardLayout>
+      {pictures.map((picture, index) => (
+        <CardThree key={index}>
+          
+        </CardThree>
+      ))}
+    </CardLayout>
+    </div>
+  );
+}
+
+const CardLayout = styled.div`
+  margin: 0px 0px 1rem;
+`;
+
+
+const CardThree = styled.div`
+  background: url(../../../src/assets/myBanners/contact.jpg) no-repeat center center;
+  background-size: cover; /* This will cover the entire container without repeating */
+  height: 350px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+`;
+
+export default ContactBanner;
--- frontend/src/pages/Contact/ContactPage.jsx
+++ frontend/src/pages/Contact/ContactPage.jsx
@@ -1,19 +1,19 @@
 import HeaderComponent from '../../components/header/header.component';
 import FooterComponent from '../../components/footer/footer.component';
 import ContactComponent from '../../components/contact/contact.component';
+import ContactBanner from '../../components/banner/contactBanner.component';
 import Container from 'react-bootstrap/Container';
 import "./ContactPageStyle.css"
 
 
 export const ContactPage = () => {
     return (
-      
-            <Container >
-                <HeaderComponent />
-                <ContactComponent />
-                <FooterComponent />
-            </Container>
- 
+        <Container fluid>      
+            <HeaderComponent />
+            <ContactBanner />
+            <ContactComponent />
+            <FooterComponent />
+        </Container>
     );
 }
 
