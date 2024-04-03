803d43b1ebfbdba506379a8273098f53867ee79f
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-04-03 14:47:24 +0100

    footer added

--- frontend/src/components/footer/footer.component.jsx
+++ frontend/src/components/footer/footer.component.jsx
@@ -1,13 +1,33 @@
 import { Nav, Navbar } from "react-bootstrap";
+import { useNavigate } from "react-router-dom"; 
+import "./footerStyle.css"
+import { useUser } from "../../contexts/UserContext";
 
 const FooterComponent = () => {
+  const { userData, refreshUserData } = useUser();
+  const navigate = useNavigate();
+
+  const logout = () => {
+    if (userData) {
+        window.localStorage.removeItem("token");
+        window.localStorage.removeItem("userid");
+        navigate("/");
+        window.location.reload()
+    } else {
+        navigate("/");
+    }
+  };
+
   return (
     <div className="footerComponent">
       <Navbar  className="custom-navbar">
             <Nav className="mr-auto">
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/shop">Shop</Nav.Link>
-              <Nav.Link href="/signup">Sign in</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+              {userData && <Nav.Link href="/profile" className="userinPlace">{userData.name}</Nav.Link>}
+                {userData ? <div onClick={logout} className="signOutFooter">Sign Out</div>
+                : <Nav.Link href="/login"> Sign in</Nav.Link>}
             </Nav>
             <Nav className="ms-auto">
               <Nav.Link href="https://github.com/gustavoperess" target="_blank" rel="noopener noreferrer">@github</Nav.Link>
