96b3b003d932400fc928fb817e93f8c17262924b
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-04-04 12:36:57 +0100

    user details form completed

--- frontend/src/components/profile/profileUserDetails.component.jsx
+++ frontend/src/components/profile/profileUserDetails.component.jsx
@@ -26,6 +26,7 @@ function ProfileUserDetails() {
         formData.append("emailAddress", emailAddress || userData.email);
         try {
             await editUsersInformation(formData, userData?._id, token);
+            location.reload()
         } catch (err) {
             console.log("error updating user information", err);
         }
@@ -40,47 +41,48 @@ function ProfileUserDetails() {
             <div>
                 {!isEditMode && (
                     <ListGroup>
-                        <ListGroup.Item>Name: {userData?.name}</ListGroup.Item>
-                        <ListGroup.Item>Email address: {userData?.email}</ListGroup.Item>
-                        <ListGroup.Item>Gender: Unspecified</ListGroup.Item>
-                        <ListGroup.Item>Phone number: Unspecified</ListGroup.Item>
-                        <ListGroup.Item>Password: *****</ListGroup.Item>
+                        <ListGroup.Item><b>Name:</b> {userData?.name}</ListGroup.Item>
+                        <ListGroup.Item><b>Email address: </b>{userData?.email}</ListGroup.Item>
+                        <ListGroup.Item><b>Gender: </b> Unspecified </ListGroup.Item>
+                        <ListGroup.Item><b>Phone number: </b>Unspecified</ListGroup.Item>
+                        <ListGroup.Item><b>Password: </b>*****</ListGroup.Item>
                         <Button variant="primary" className="btn btn-dark" onClick={toggleEditMode}>EDIT</Button>
                     </ListGroup>
                 )}
                 {isEditMode && (
-                    <Form  >
-                        <Form.Group className="mb-3" >
+                 <Form>
+                        <Form.Group className="mb-3 form-group-row">
                             <Form.Label>Name</Form.Label>
                             <Form.Control 
                                 type="text" 
                                 defaultValue={userData?.name}
                                 onChange={(e) => setUsername(e.target.value)}
-                                />
+                            />
                         </Form.Group>
-                        <Form.Group className="mb-3" >
+                        <Form.Group className="mb-3 form-group-row">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control 
                                 type="email"
+                                className="wide-input" 
                                 defaultValue={userData?.email}
                                 onChange={(e) => setEmailAddress(e.target.value)}
-                                />
+                            />
                         </Form.Group>
-                        <Form.Group className="mb-3" >
+                        <Form.Group className="mb-3 form-group-row">
                             <Form.Label>Password</Form.Label>
                             <Form.Control 
                                 type="text" 
                                 defaultValue={userData?.password}
                                 onChange={(e) => setPassword(e.target.value)}
-                                />
+                            />
                         </Form.Group>
-                        <Button variant="primary" type="submit" onClick={handleOnSubmit} >
+                        <Button  className="btn btn-dark" type="submit" onClick={handleOnSubmit}>
                             Submit
                         </Button>
-                        <Button variant="secondary" onClick={toggleEditMode} style={{marginLeft: '10px'}}>
+                        <Button  className="btn btn-dark" onClick={toggleEditMode} style={{marginLeft: '10px'}}>
                             Cancel
                         </Button>
-                    </Form>
+                </Form>
                 )}
             </div>
         </div>
