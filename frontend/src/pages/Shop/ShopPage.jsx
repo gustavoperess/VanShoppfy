eed4cf51db1bda8a378fe780d0f34e631e47d460
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-04-04 17:47:25 +0100

    changes

--- frontend/src/pages/Shop/ShopPage.jsx
+++ frontend/src/pages/Shop/ShopPage.jsx
@@ -4,7 +4,7 @@ import ShopPageComponent from '../../components/shoppagecard/shopagecard.compone
 import ContactUs from '../../components/contact/contactus.component';
 import Container from 'react-bootstrap/Container';
 import FooterComponent from '../../components/footer/footer.component';
-import SidebarComponent from '../../components/sidebar/sideeBarCardd.component';
+import SidebarComponent from '../../components/sidebar/sidebar.component';
 
 import "./ShopPageStyle.css"
 
