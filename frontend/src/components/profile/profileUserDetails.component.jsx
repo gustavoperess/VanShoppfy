9bb70dbf609acf1fdbf06fd5b124b4508f5b6966
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-04-04 11:43:06 +0100

    form added

--- frontend/src/components/profile/profileUserDetails.component.jsx
+++ frontend/src/components/profile/profileUserDetails.component.jsx
@@ -1,12 +1,16 @@
+import React, { useState } from 'react'; // Import useState
 import { useUser } from "../../contexts/UserContext";
 import SideListComponent from './sideList.component';
-import {ListGroup, Button} from 'react-bootstrap';
-import "./profileStyle.css"
+import {ListGroup, Button, Form} from 'react-bootstrap';
+import "./profileStyle.css";
 
 function ProfileUserDetails() {
-    const { userData, refreshUserData } = useUser();
-    const token = window.localStorage.getItem("token")
- 
+    const { userData } = useUser();
+    const [isEditMode, setIsEditMode] = useState(false);
+
+    const toggleEditMode = () => {
+        setIsEditMode(!isEditMode);
+    };
 
     return (
         <div className='profile-page-container'>
@@ -14,18 +18,42 @@ function ProfileUserDetails() {
                 <SideListComponent />
             </div>
             <div>
-            <ListGroup>
-                <ListGroup.Item>Name: {userData?.name}</ListGroup.Item>
-                <ListGroup.Item>Email address:   {userData?.email} </ListGroup.Item>
-                <ListGroup.Item>Gender: Unspecified</ListGroup.Item>
-                <ListGroup.Item>Phone number: Unspecified</ListGroup.Item>
-                <ListGroup.Item>Password: {userData?.password}</ListGroup.Item>
-                <Button variant="primary" className="btn btn-dark">EDIT</Button>
-            </ListGroup>
+                {!isEditMode && (
+                    <ListGroup>
+                        <ListGroup.Item>Name: {userData?.name}</ListGroup.Item>
+                        <ListGroup.Item>Email address: {userData?.email}</ListGroup.Item>
+                        <ListGroup.Item>Gender: Unspecified</ListGroup.Item>
+                        <ListGroup.Item>Phone number: Unspecified</ListGroup.Item>
+                        <ListGroup.Item>Password: *****</ListGroup.Item>
+                        <Button variant="primary" className="btn btn-dark" onClick={toggleEditMode}>EDIT</Button>
+                    </ListGroup>
+                )}
+                {isEditMode && (
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" defaultValue={userData?.name} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" defaultValue={userData?.email} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                        <Button variant="secondary" onClick={toggleEditMode} style={{marginLeft: '10px'}}>
+                            Cancel
+                        </Button>
+                    </Form>
+                )}
             </div>
-         
         </div>
-      );
+    );
 }
 
 export default ProfileUserDetails;
+``
