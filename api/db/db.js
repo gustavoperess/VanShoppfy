6f5828939e785f663116d9c792dc13d46b7f0de2
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-04-06 12:36:44 +0100

    changes in the database connection

--- api/db/db.js
+++ api/db/db.js
@@ -1,18 +1,27 @@
 const mongoose = require("mongoose");
 
+function keepDbAlive() {
+  mongoose.connection.db.command({ ping: 1 })
+    .then(() => console.log("DB ping successful"))
+    .catch((err) => console.error("DB ping failed:", err));
+}
+
 const connectToDatabase = async () => {
   const mongoDbUrl = process.env.MONGODB_URL;
 
   if (!mongoDbUrl) {
-    console.error(
-      "No MongoDB url provided. Make sure there is a MONGODB_URL environment variable set. See the README for more details."
-    );
+    console.error("No MongoDB url provided...");
     throw new Error("No connection string provided");
   }
 
-  await mongoose.connect(mongoDbUrl);
-  if (process.env.NODE_ENV !== "test") {
+  try {
+    await mongoose.connect(mongoDbUrl);
     console.log("Successfully connected to MongoDB");
+
+
+    setInterval(keepDbAlive, 600000); 
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error.message);
   }
 };
 
