24719f432cbca9aae8dc3a0284982b45c7057f7f
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-09-29 21:40:11 +0100

    adding cors correct link

--- api/app.js
+++ api/app.js
@@ -11,7 +11,7 @@ const app = express();
 const cookieParser = require('cookie-parser');
 
 const corsOptions = {
-  origin: 'https://wonderful-desert-04ea24410.5.azurestaticapps.net/', // Replace with your frontend's origin https://vanshoppfy.onrender.com http://localhost:5173
+  origin: 'https://wonderful-desert-04ea24410.5.azurestaticapps.net', // Replace with your frontend's origin https://vanshoppfy.onrender.com http://localhost:5173
   credentials: true, // This is important to allow cookies
 };
 
