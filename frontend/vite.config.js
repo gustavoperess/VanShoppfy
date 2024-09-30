6a3a1e464c18301daf4b6c7800d463ef2068db5c
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-09-30 13:54:06 +0100

    adding a vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import history from 'connect-history-api-fallback'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middlewareMode: 'html',
+    setup: (server) => {
+      server.middlewares.use(history({
+        rewrites: [
+          { from: /\/.*\.(html|js|css|png|jpg|jpeg|gif|svg|ico|json)$/, to: context => context.parsedUrl.pathname }
+        ]
+      }))
+    }
+  }
 })
