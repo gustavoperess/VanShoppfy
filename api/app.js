d1b952c6dc7374dce3ca7b9fbab0fd4d8673b844
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-10-01 15:02:38 +0100

    commenting out the cors since is server to server request

--- api/app.js
+++ api/app.js
@@ -11,34 +11,34 @@ const app = express();
 const cookieParser = require('cookie-parser');
 
 // Define CORS options
-const allowedOrigins = [
-  'https://mango-forest-09f2f4f03.5.azurestaticapps.net',
-  // "http://localhost:5173"
-];
+// const allowedOrigins = [
+//   'https://mango-forest-09f2f4f03.5.azurestaticapps.net',
+//   // "http://localhost:5173"
+// ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log(`Incoming request with origin: ${origin}`);
-    if (!origin) {
-      console.log(`CORS request allowed from origin: undefined (likely server-to-server request)`);
-      return callback(null, true);
-    }
-    if (allowedOrigins.includes(origin)) {
-      console.log(`CORS request allowed from origin: ${origin}`);
-      return callback(null, true);
-    } else {
-      console.log(`Blocked by CORS: ${origin}`);
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // specify allowed methods
-  allowedHeaders: ["Content-Type", "Authorization"], // specify allowed headers
-  credentials: true, // Allow cookies
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     console.log(`Incoming request with origin: ${origin}`);
+//     if (!origin) {
+//       console.log(`CORS request allowed from origin: undefined (likely server-to-server request)`);
+//       return callback(null, true);
+//     }
+//     if (allowedOrigins.includes(origin)) {
+//       console.log(`CORS request allowed from origin: ${origin}`);
+//       return callback(null, true);
+//     } else {
+//       console.log(`Blocked by CORS: ${origin}`);
+//       return callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // specify allowed methods
+//   allowedHeaders: ["Content-Type", "Authorization"], // specify allowed headers
+//   credentials: true, // Allow cookies
+// };
 
 
-// Enable CORS
-app.use(cors(corsOptions));
+// // Enable CORS
+// app.use(cors(corsOptions));
 
 // Log all incoming requests
 app.use((req, res, next) => {
